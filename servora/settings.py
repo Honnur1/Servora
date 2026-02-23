4a18fe662c4bb991593048fb253d79dec73c49bb
Honnur M <honnurm39@gmail.com>
2026-02-23 17:50:34 +0530

    Update settings.py
    DEBUG = true
    
    ALLOWED_HOSTS = []
    to 
    DEBUG = False
    
    ALLOWED_HOSTS = ['*']

--- servora/settings.py
+++ servora/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8xl68y87b8yd$bh*!%h&2&3)58pe!#i@kha82g!e*akyc=_-*d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [['*']]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
